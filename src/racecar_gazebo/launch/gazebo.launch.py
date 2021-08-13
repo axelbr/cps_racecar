b00a0cc51400a87259fa2b7e2908d03e087a07e1
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2021-08-13 07:59:40 +0000

    fixed launch file

--- src/racecar_gazebo/launch/gazebo.launch.py
+++ src/racecar_gazebo/launch/gazebo.launch.py
@@ -4,25 +4,30 @@ import launch_ros
 import os
 
 def generate_launch_description():
-    pkg_description = launch_ros.substitutions.FindPackageShare(package='racecar_description').find('racecar_description')
-    pkg_gazebo = launch_ros.substitutions.FindPackageShare(package='racecar_gazebo').find('racecar_gazebo')
-    default_model_path = os.path.join(pkg_description, 'urdf/racecar.urdf.xacro')
-    default_rviz_config_path = os.path.join(pkg_description, 'rviz/urdf_config.rviz')
-    world_path=os.path.join(pkg_gazebo, 'worlds/silverstone_track.sdf')
-    
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='racecar_description').find('racecar_description')
+    default_model_path = os.path.join(pkg_share, 'urdf/racecar.urdf.xacro')
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+
+    gazebo_pkg = launch_ros.substitutions.FindPackageShare(package='racecar_gazebo').find('racecar_gazebo')
+    world_path = os.path.join(gazebo_pkg, 'worlds', 'silverstone.sdf')
+
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
-    
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
-
+    joint_state_publisher_gui_node = launch_ros.actions.Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+    )
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
@@ -44,14 +49,17 @@ def generate_launch_description():
                                             description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
+        launch.actions.DeclareLaunchArgument(name='track', default_value=world_path,
+                                            description='Absolute path to SDF file.'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
                                             description='Flag to enable use_sim_time'),
-        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
+        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so', LaunchConfiguration('track')], output='screen'),
 
-        joint_state_publisher_node,
         robot_state_publisher_node,
-        spawn_entity,
-       
+        joint_state_publisher_gui_node,
+        joint_state_publisher_node,
+        rviz_node,
+        spawn_entity
     ])
