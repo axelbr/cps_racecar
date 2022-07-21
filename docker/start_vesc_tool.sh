22251d77c4a3541d3c24a404e75a75f4cc61d9cf
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2022-07-21 13:07:25 +0000

    update configs

--- docker/start_vesc_tool.sh
+++ docker/start_vesc_tool.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo xhost +local:docker
-docker run --rm -it --privileged --volume=/tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --network host vesc_tool ./vesc_tool_3.00
+docker run --rm -it --privileged --volume=/tmp/.X11-unix:/tmp/.X11-unix -v /dev:/dev -e DISPLAY=$DISPLAY --network host vesc_tool ./vesc_tool_3.00
 
