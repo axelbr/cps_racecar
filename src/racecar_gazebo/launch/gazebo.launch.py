876b4446772fdc3cfc94c5b63b9fc7a0f42cb146
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2022-08-19 14:01:34 +0200

    added additional urdf models

--- src/racecar_gazebo/launch/gazebo.launch.py
+++ src/racecar_gazebo/launch/gazebo.launch.py
@@ -5,7 +5,7 @@ import os
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='racecar_description').find('racecar_description')
-    default_model_path = os.path.join(pkg_share, 'urdf/racecar.urdf.xacro')
+    default_model_path = os.path.join(pkg_share, 'urdf/drift_car.urdf.xacro')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
 
     gazebo_pkg = launch_ros.substitutions.FindPackageShare(package='racecar_gazebo').find('racecar_gazebo')
@@ -14,21 +14,14 @@ def generate_launch_description():
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[{'robot_description': launch_ros.parameter_descriptions.ParameterValue(Command(['xacro ', LaunchConfiguration('model')]), value_type=str)}]
     )
+
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
-    )
-
-    rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
 
     spawn_entity = launch_ros.actions.Node(
@@ -53,6 +46,5 @@ def generate_launch_description():
 
         robot_state_publisher_node,
         joint_state_publisher_node,
-        rviz_node,
         spawn_entity
     ])
